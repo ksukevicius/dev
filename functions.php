d836b92f1860523e0616800eabc98427f4e4c378
Kestas <k.sukevicius@gmail.com>
2020-06-17 09:38:45 +0300

    some new functions

--- functions.php
+++ functions.php
@@ -5,4 +5,47 @@ function test_input($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+function resize_image($file, $w, $h, $crop=FALSE) {
+	list($width, $height) = getimagesize($file);
+	$r = $width / $height;
+	if ($crop) {
+		if ($width > $height) {
+			$width = ceil($width-($width*abs($r-$w/$h)));
+		} else {
+			$height = ceil($height-($height*abs($r-$w/$h)));
+		}
+		$newwidth = $w;
+		$newheight = $h;
+	} else {
+		if ($w/$h > $r) {
+			$newwidth = $h*$r;
+			$newheight = $h;
+		} else {
+			$newheight = $w/$r;
+			$newwidth = $w;
+		}
+	}
+	$src = imagecreatefromjpeg($file);
+	$dst = imagecreatetruecolor($newwidth, $newheight);
+	imagecopyresampled($dst, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+	
+	//To save the resized image in the file system add imagejpeg($dst, $file); 
+	//after the imagecopyresampled($dst,... line. Change $file if you don't want to overwrite the original. 
+	$newFileName = "Resized.jpeg";
+	imagejpeg($dst, $newFileName);
+
+	return $dst;
+}
+
 ?>
